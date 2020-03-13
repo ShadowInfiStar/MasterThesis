2e03468ad8b7db0e3c37500e609885985a3de939
Birte De Smedt <birte.desmedt@gmail.com>
2020-03-13 21:54:33 +0100

    removes Blank Nodes from rdf graph and test on finding a subjectMap using rdflib.Namespace

--- Parse.py
+++ Parse.py
@@ -1,13 +1,41 @@
 import rdflib
 
+def printGraph(g):
+    for stmt in g:
+        print(stmt)
 
-print(rdflib.util.guess_format("RML.ttl"))
+#print(rdflib.util.guess_format("RML.ttl"))
 
 g = rdflib.Graph()
-g.parse("RML.ttl", format="turtle")
+#g.parse("RML.ttl", format="turtle")
+g.parse("C:\\Users\\Birte\\Documents\\GitHub\\rml-test-cases\\test-cases\\RMLTC0000-CSV\\mapping.ttl", format="turtle")
+
 
 len(g) # prints 2
 
-import pprint
-for stmt in g:
-    pprint.pprint(stmt)
+#import pprint
+#for stmt in g:
+#    pprint.pprint(stmt)
+
+#for stmt in g:
+#    print(stmt)
+
+for s,p,o in g:
+    for s2,p2,o2 in g:
+        if o == s2:
+            g.add((p,p2,o2))
+            g.remove((s2,p2,o2))
+            g.remove((s,p,o))
+#wat als we meerdere triplemaps hebben?? testen op eerste subject en daaruit naam triples map halen
+
+#from rdflib import Namespace
+rml = rdflib.Namespace('http://semweb.mmlab.be/ns/rml#')
+r2rml = rdflib.Namespace('http://www.w3.org/ns/r2rml#')
+n = rdflib.Namespace("http://example.org/")
+s = r2rml.subjectMap
+
+for s,p,o in g.triples((s,None,None)):
+    print(s,p,o)
+
+
+
