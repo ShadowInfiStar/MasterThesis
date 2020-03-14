f40debae47e913f2a4e057b42a608718f36d20a6
Birte De Smedt <birte.desmedt@gmail.com>
2020-03-14 15:09:49 +0100

    made class from parsing the RML file

--- RML.py
+++ RML.py
@@ -1,41 +1,37 @@
 import rdflib
+import pprint
 
-def printGraph(g):
-    for stmt in g:
-        print(stmt)
+class RML:
+    def __init__(self):
+        self.graph = rdflib.Graph()
+    def printGraph(self, keuze):
+        if keuze == 1: 
+            for stmt in self.graph:
+                print(stmt)
+        else:
+            for stmt in self.grap:
+                pprint.pprint(stmt)
+    def createGraph(self):
+        self.graph.parse("C:\\Users\\Birte\\Documents\\GitHub\\rml-test-cases\\test-cases\\RMLTC0000-CSV\\mapping.ttl", format="turtle")
+    def removeBlankNodes(self):
+        for s,p,o in self.graph:
+            for s2,p2,o2 in self.graph:
+                if o == s2:
+                    self.graph.add((p,p2,o2))
+                    self.graph.remove((s2,p2,o2))
+                    self.graph.remove((s,p,o))
+
+#wat als we meerdere triplemaps hebben?? testen op eerste subject en daaruit naam triples map halen
+
+
 
 #print(rdflib.util.guess_format("RML.ttl"))
 
-g = rdflib.Graph()
+#g = rdflib.Graph()
 #g.parse("RML.ttl", format="turtle")
-g.parse("C:\\Users\\Birte\\Documents\\GitHub\\rml-test-cases\\test-cases\\RMLTC0000-CSV\\mapping.ttl", format="turtle")
+#g.parse("C:\\Users\\Birte\\Documents\\GitHub\\rml-test-cases\\test-cases\\RMLTC0000-CSV\\mapping.ttl", format="turtle")
 
-
-len(g) # prints 2
-
-#import pprint
-#for stmt in g:
-#    pprint.pprint(stmt)
-
-#for stmt in g:
-#    print(stmt)
-
-for s,p,o in g:
-    for s2,p2,o2 in g:
-        if o == s2:
-            g.add((p,p2,o2))
-            g.remove((s2,p2,o2))
-            g.remove((s,p,o))
-#wat als we meerdere triplemaps hebben?? testen op eerste subject en daaruit naam triples map halen
 
 #from rdflib import Namespace
-rml = rdflib.Namespace('http://semweb.mmlab.be/ns/rml#')
-r2rml = rdflib.Namespace('http://www.w3.org/ns/r2rml#')
-n = rdflib.Namespace("http://example.org/")
-s = r2rml.subjectMap
-
-for s,p,o in g.triples((s,None,None)):
-    print(s,p,o)
-
 
 
